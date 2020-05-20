6464836fb6018cd46e58d212b1c7d92ea4b30c53
allamimouad <allamimouad@gmail.com>
2020-05-20 14:08:06 +0000

    adding user_read and user_owner_read to create reservation

--- locatoria/app/Http/Controllers/ReservationController.php
+++ locatoria/app/Http/Controllers/ReservationController.php
@@ -34,6 +34,8 @@ class ReservationController extends Controller
         $res->date_end = request('date_end');
         $res->total_price = request('total_price');
         $res->item_id = request('item_id');
+        $res->user_read = 1;
+        $res->user_owner_read = 0;
         $res->user_id = Auth::user()->id;
         $res->user_owner_id = Item::find(request('item_id'))->user->id;
 
