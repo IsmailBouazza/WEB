bf153ce3c7d035574c5cb6ce99fcc5cc1f31f25a
allamimouad <allamimouad@gmail.com>
2020-05-26 23:08:18 +0000

    fixing nav bar -_-

--- locatoria/resources/views/admin/premium.blade.php
+++ locatoria/resources/views/admin/premium.blade.php
@@ -6,13 +6,99 @@
 @section('content')
 
 <br><br><br>
-    
-   
-    
+
+
+<style>
+
+
+    /*new tag css*/
+
+    .tags span {
+        display: inline-block;
+        height:24px;
+        line-height:23px;
+        position:relative;
+        margin: 0 12px 8px 0;
+        padding: 0 12px 0 10px;
+        background: #777;
+        -moz-border-radius-bottomleft: 5px;
+        -webkit-border-bottom-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+        -moz-border-radius-topleft: 5px;
+        -webkit-border-top-left-radius: 5px;
+        border-top-left-radius: 5px;
+        box-shadow: 0 1px 2px rgba(0,0,0,0.2);
+        color: #fff;
+        font-size:12px;
+        font-family: "Lucida Grande","Lucida Sans Unicode",Verdana,sans-serif;
+        text-decoration: none;
+        text-shadow: 0 1px 2px rgba(0,0,0,0.2);
+        font-weight: bold;
+    }
+
+    .tags span:before {
+        content: "";
+        position: absolute;
+        top: 10px;
+        right: 1px;
+        float: left;
+        width: 5px;
+        height: 5px;
+        -moz-border-radius: 50%;
+        -webkit-border-radius: 50%;
+        border-radius: 50%;
+        background: #fff;
+        -moz-box-shadow: -1px -1px 2px rgba(0,0,0,0.4);
+        -webkit-box-shadow: -1px -1px 2px rgba(0,0,0,0.4);
+        box-shadow: -1px -1px 2px rgba(0,0,0,0.4);
+    }
+
+    .tags span:after {
+        content: "";
+        position: absolute;
+        top:0;
+        right: -12px;
+        width: 0;
+        height: 0;
+        border-color: transparent transparent transparent #777;
+        border-style: solid;
+        border-width: 12px 0 12px 12px;
+    }
+
+    .tags span.color3 {background: #de3f3e;}
+    .tags span.color3:after {border-color: transparent transparent transparent #de3f3e}
+
+    .small span {
+        height: 21px;
+        line-height: 21px;
+        float: none;
+        font-size: 11px;
+    }
+
+    .small span:before {
+        right: 0;
+        top: 8px;
+        border-width: 10px 0 10px 10px;
+    }
+
+    .small span:after {
+        right: -11px;
+        top: 0;
+        border-width: 11px 0 11px 11px;
+    }
+
+    /*end new tag css*/
+
+
+
+</style>
+
+
+
     <div class="title" style=" margin-left : 30%; display : flex; flex-wrap : wrap; text-align: center; margin-top : 2%;">
         <i class="fas fa-dollar-sign fa-5x "></i>
         <h1 style="margin-top : 15px; margin-left : 2%;;">Premium requests</h1>
-    
+
     </div>
     <hr>
     <div class="reservations">
@@ -20,11 +106,11 @@
     @forelse($data as $premium)
 
             <div class="res-container" style=" width: 80%;">
-              
+
                 <div class="card flex1" style="width: 150px; height: 150px; border-radius: 50%">
-                    <img src="{{asset('/storage/' .$premium->thumbnail_path )}}" style="border-radius: 50%" class="bd-placeholder-img card-img-top" width="150px" height="150px" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" role="img"> 
+                    <img src="{{asset('/storage/' .$premium->thumbnail_path )}}" style="border-radius: 50%" class="bd-placeholder-img card-img-top" width="150px" height="150px" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" role="img">
                 </div>
-                
+
                  <div class="flex2" style="width: 150px;">
                     <div style="width:100%;  text-align:center;">
                         <span style="font-size: 1.5em"><u>Owner :</u><br></span>
@@ -34,13 +120,13 @@
                     </div>
 
                 </div>
-               
+
                 <div class="flex3" style="width: 150px;">
                     <div style="width:100%; height:10px; text-align:center;">
                         <span style="font-size: 1.5em"><u>Item :</u><br></span>
                         <span style="font-size: 1em">
                             Title : {{$premium->title}}<br>
-                           
+
                         </span>
                     </div>
                     <div style="width:100%; font-size: 1.5em; height:10px; text-align:center; margin-top:80px">
@@ -53,7 +139,7 @@
                     @if($premium->status == 0)
                         <br>
                         <span style="color: #000000; font-weight: bold; margin-top: 10px">Accept the request ?</span><br><br>
-                        
+
                         <button type="button" class="btn btn-primary" onclick="approveRequest({{ $premium->id }})">Accept</button>
                         <form method="post" action="{{ url('premium/' .$premium->id. '/approve') }}" id="approval-form" style="display: none">
                             @csrf
@@ -69,31 +155,31 @@
                         <span style="color: #000000; font-weight: bold; display: flex; flex-wrap : wrap;" ><h5>It is premium now !!</h5><i class="far fa-smile-wink fa-3x" style="text-align : center;"></i></span>
                     @endif
                 </div>
-               
+
                 @empty
 
                 <div class="msg">
                     <p class="msg">No requests found</p>
-                    <small>Sorry try latter !!</small> 
+                    <small>Sorry try latter !!</small>
                 </div>
 
             </div>
-           
+
         @endforelse
 
 
         </div>
-    
+
 
 
     <!-- accept or refuse reservation with js -->
-    
+
     <!-- Select Plugin Js -->
     <script src="{{ asset('assets/backend/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
     <!-- TinyMCE -->
     <script src="{{ asset('assets/backend/plugins/tinymce/tinymce.js') }}"></script>
     <script src="https://unpkg.com/sweetalert2@7.19.1/dist/sweetalert2.all.js"></script>
-    
+
     <script>
         $(function () {
             //TinyMCE
